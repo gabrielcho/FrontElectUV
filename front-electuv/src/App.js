837758a933ee4567d80260214cee9d41ed048544
auraPasmin <1288@holbertonschool.com>
2021-10-14 16:33:34 -0500

    fix css

--- front-electuv/src/App.js
+++ front-electuv/src/App.js
@@ -6,6 +6,8 @@ import PageBar from './components/PageBar'
 import CoursePage from './templates/CoursePage';
 import CourseCardBox from './components/CourseCardBox';
 import Container from '@mui/material/Container';
+import Grid from "@mui/material/Grid";
+import Review from './components/Review';
 
 
 function App() {
@@ -15,20 +17,41 @@ function App() {
 return (
   <Container>
   <Router>
-
+  <Grid
+  container
+  direction="column"
+  justifyContent="flex-start"
+  alignItems="stretch"
+>
     <div className='App'>
       <PageBar/>
+      <Container sx={{ m: 3 }}>
       <div className='content'>
         <Route exact path='/'>
-          <CourseCardBox/>
+        <Grid container spacing={2}>
+        <Grid item xs={6} md={8}>
+    <Review/>
+    </Grid>
+        <Grid item  xs={6} md={4}>
+       <CourseCard/>
+
+   </Grid>
+    <Grid item xs={6} md={4}>
+    <CourseCardBox/>
+    </Grid>
+
+    </Grid>
+         
+         
         </Route>
 
         <Route path='/curso/:id'>
           <CoursePage/>
         </Route>
       </div>    
-  
+      </Container>
       </div>
+    </Grid>  
   </Router>
   </Container>
 );
